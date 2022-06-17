12a9ebb85a67392c7be836f4e2c2fff2eddc8461
unknown <viniciusdeab@outlook.com>
2022-06-17 16:04:03 -0300

    Adição da dinaminca com javascript para o menu lateral

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ const menu = document.querySelector('.menu-lateral');
 botaoMenu.addEventListener('click', () => {
     menu.classList.toggle('menu-lateral--ativo');
 })
+
+const list = document.querySelectorAll('.menu-lateral__link');
+            function activeLink(){
+                list.forEach((item) => 
+                item.classList.remove('menu-lateral__link--ativo'));
+                this.classList.add('menu-lateral__link--ativo');                
+            }
+            list.forEach((item)=>
+            item.addEventListener('click', activeLink));
